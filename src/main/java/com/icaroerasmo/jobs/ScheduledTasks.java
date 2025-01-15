6cfd7c624dfd1c7b8edf26c1072de1055490c1ea
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2025-01-15 01:49:04 -0300

    fixes bug that made scheduler run twice

---
+++ src/main/java/com/icaroerasmo/jobs/ScheduledTasks.java
@@ -0,0 +1,66 @@
+package com.icaroerasmo.jobs;
+
+import com.icaroerasmo.properties.JavaRtspProperties;
+import com.icaroerasmo.properties.RtspProperties;
+import com.icaroerasmo.properties.StorageProperties;
+import com.icaroerasmo.util.FfmpegUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class ScheduledTasks {
+
+    private final FfmpegUtil ffmpegUtil;
+    private final JavaRtspProperties javaRtspProperties;
+
+    @Scheduled(fixedDelayString =
+            "#{@propertiesUtil.durationParser(" +
+            "@storageProperties.fileMoverSleep, " +
+            "T(java.util.concurrent.TimeUnit).MILLISECONDS)}")
+    private void filesMover() {
+
+        log.info("Started job to move files to records folder");
+
+        final StorageProperties storageProperties = javaRtspProperties.getStorageProperties();
+        final List<RtspProperties.Camera> cameras = javaRtspProperties.getRtspProperties().getCameras();
+
+        cameras.forEach(cam -> {
+            final Path tmpFolder = Paths.get(storageProperties.getTmpFolder());
+            final Path segmentsFile = tmpFolder.resolve(".%s_done_segments".formatted(cam.getName()));
+            try {
+
+                List<String> fileList = Files.readAllLines(segmentsFile);
+
+                if(fileList.isEmpty()) {
+                    return;
+                }
+
+                ffmpegUtil.moveFilesToRecordsFolder(fileList);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // Clear done segments file
+            try {
+                new PrintWriter(segmentsFile.toFile()).close();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        log.info("Finished job to move files to records folder");
+    }
+}
