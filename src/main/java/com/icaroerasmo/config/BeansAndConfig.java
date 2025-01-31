f62418f001ae7a26b31bae67930761dfd57211a8
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2025-01-31 14:30:49 -0300

    fix: checks if there is value before setting locale and timezone

--- src/main/java/com/icaroerasmo/config/BeansAndConfig.java
+++ src/main/java/com/icaroerasmo/config/BeansAndConfig.java
@@ -8,6 +8,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import java.util.Locale;
 import java.util.TimeZone;
@@ -23,8 +24,8 @@ public class BeansAndConfig {
 
     @PostConstruct
     public void init() {
-        Locale.setDefault(Locale.forLanguageTag(generalProperties.getLocale()));
-        TimeZone.setDefault(TimeZone.getTimeZone(generalProperties.getTimezone()));
+        setLocale();
+        setTimezone();
     }
 
     @Bean
@@ -35,5 +36,17 @@ public class BeansAndConfig {
     @Bean
     public TelegramBot bot() {
         return new TelegramBot(telegramProperties.getBotToken());
+    }
+
+    private void setLocale() {
+        if(StringUtils.hasText(generalProperties.getLocale())) {
+            Locale.setDefault(Locale.forLanguageTag(generalProperties.getLocale()));
+        }
+    }
+
+    private void setTimezone() {
+        if(StringUtils.hasText(generalProperties.getTimezone())) {
+            TimeZone.setDefault(TimeZone.getTimeZone(generalProperties.getTimezone()));
+        }
     }
 }
