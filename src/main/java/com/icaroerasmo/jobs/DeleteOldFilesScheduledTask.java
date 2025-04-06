f83867068cf5986b5f5b2639b14dd42cb4227ab4
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2025-04-06 00:52:09 -0300

    fix: deletes files older than oldest file in index

--- src/main/java/com/icaroerasmo/jobs/DeleteOldFilesScheduledTask.java
+++ src/main/java/com/icaroerasmo/jobs/DeleteOldFilesScheduledTask.java
@@ -1,6 +1,7 @@
 package com.icaroerasmo.jobs;
 
 import com.icaroerasmo.properties.StorageProperties;
+import com.icaroerasmo.util.FfmpegUtil;
 import com.icaroerasmo.util.PropertiesUtil;
 import com.icaroerasmo.util.Utilities;
 import lombok.RequiredArgsConstructor;
@@ -13,30 +14,38 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
+
+import static com.icaroerasmo.util.FfmpegUtil.INDEX;
 
 @Log4j2
 @Component
 @RequiredArgsConstructor
 public class DeleteOldFilesScheduledTask {
 
+    private final FfmpegUtil ffmpegUtil;
     private final PropertiesUtil propertiesUtil;
     private final StorageProperties storageProperties;
-    private final Utilities utilities;
 
     @Scheduled(cron = "#{@storageProperties.deleteOldFilesCron}")
     private void rcloneDedupe() throws IOException {
-        log.info("Started deleting files older than {}", utilities.getFullTimeAmount(getTimeInMillis()));
+        final ZonedDateTime lastModified = findOldestModifiedDate();
+        log.info("Started deleting files older than {}", lastModified.format(DateTimeFormatter.ISO_ZONED_DATE_TIME));
         final Path recordsFolder = Paths.get(storageProperties.getRecordsFolder());
-        folderDeleter(recordsFolder);
-        log.info("Finished deleting files older than {}", utilities.getFullTimeAmount(getTimeInMillis()));
+        folderDeleter(lastModified, recordsFolder);
+        log.info("Finished deleting files older than {}", lastModified.format(DateTimeFormatter.ISO_ZONED_DATE_TIME));
     }
 
-    private void deleteOldFiles(Path path) throws IOException {
+    private void deleteOldFiles(ZonedDateTime lastModified, Path path) throws IOException {
         if(isFolder(path)) {
-            folderDeleter(path);
+            folderDeleter(lastModified, path);
             if(isEmptyFolder(path)) {
                 log.info("Deleting empty folder: {}", path);
                 Files.delete(path);
@@ -44,7 +53,7 @@ public class DeleteOldFilesScheduledTask {
             }
         } else {
             BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
-            if(isOlderThanMaxAge(attrs)) {
+            if(isOlderThanOldestModifiedInIndex(attrs, lastModified)) {
                 log.info("Deleting old file: {}", path);
                 Files.delete(path);
                 log.info("Deleted old file: {}", path);
@@ -52,11 +61,11 @@ public class DeleteOldFilesScheduledTask {
         }
     }
 
-    private void folderDeleter(Path path) throws IOException {
+    private void folderDeleter(ZonedDateTime lastModified, Path path) throws IOException {
         try(Stream<Path> files = Files.list(path)) {
             files.forEach(file -> {
                 try {
-                    deleteOldFiles(file);
+                    deleteOldFiles(lastModified, file);
                 } catch (IOException e) {
                     log.error("Old files Deletion job: Error deleting file: {}", file);
                 }
@@ -72,14 +81,41 @@ public class DeleteOldFilesScheduledTask {
         return isFolder(path) && Objects.requireNonNull(path.toFile().list()).length == 0;
     }
 
-    private boolean isOlderThanMaxAge(BasicFileAttributes attrs) {
-        return Objects.nonNull(attrs) &&
-                attrs.creationTime().toMillis() <
-                        System.currentTimeMillis() - getTimeInMillis();
+    private boolean isOlderThanOldestModifiedInIndex(BasicFileAttributes attrs, ZonedDateTime lastModified) {
+
+        final ZonedDateTime fileLastModified =
+                attrs.lastModifiedTime().toInstant().
+                        atZone(ZoneId.systemDefault());
+
+        return fileLastModified.isBefore(lastModified);
     }
 
-    private Long getTimeInMillis() {
-        return Long.parseLong(propertiesUtil.durationParser(
-                storageProperties.getMaxAgeLocalVideoFiles(), TimeUnit.MILLISECONDS));
+    private ZonedDateTime findOldestModifiedDate() throws IOException {
+
+        ReentrantLock lock = ffmpegUtil.getIndexFileLock();
+
+        final Path recordsFolder = Paths.get(storageProperties.getRecordsFolder());
+        final Path indexFile = recordsFolder.resolve(INDEX);
+
+        ZonedDateTime lastModified;
+
+        try {
+            lock.lock();
+
+            final String fileRecord = Files.readAllLines(indexFile).getFirst();
+            String[] fileData = fileRecord.split(",");
+
+            String dateTime = fileData[fileData.length-1];
+            lastModified = Instant.parse(dateTime).atZone(ZoneId.systemDefault());
+
+        } catch (Exception e) {
+            log.error("Error reading index file: {}", e.getMessage());
+            log.debug("Error reading index file: {}", e.getMessage(), e);
+            throw e;
+        } finally {
+            lock.unlock();
+        }
+
+        return lastModified;
     }
 }
